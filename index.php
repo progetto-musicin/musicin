ff3292b2dd74eabf60d8701b73d7de0c7a1c2c4e
Simone Magnani <simone.magnani7@studio.unibo.it>
2024-06-12 16:47:25 +0200

    update index.php and html templates

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
+<?php
 
+// require_once 'bootstrap.php';
 
-<!--
-controlla i cookie:
- se loggato -> feed
- se non loggato -> login
- -->
+/* controlla i cookie:
+- se loggato -> feed
+- se non loggato -> login */
+
+// require 'template/base.php';
+require_once 'template/index.html';
+
+?>
