d2bba8941c71fb41fe7c384d9bb9f871bd89db70
buldra2000 <buldra00@icloud.com>
2024-06-18 16:52:51 +0200

    Aggiunta barra di ricerca username/genere.

--- template/base.php
+++ template/base.php
@@ -1,14 +1,11 @@
 <!DOCTYPE html>
 <html lang="it">
 <head>
-    <meta charset='utf-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $templateParams["title"]; ?></title>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel='stylesheet' type='text/css' media='screen' href='css/style-index.css'>
-    <script src='js/main.js'></script>
 </head>
 <body class="bg-light">
     <div class="container-fluid">
@@ -20,13 +17,17 @@
         </header>
 
         <nav>
-            <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <form class="d-flex" role="search" action="search.php" method="get">
+                <select class="form-select me-2" name="search_type" aria-label="Tipo di ricerca">
+                    <option value="username">Username</option>
+                    <option value="genre">Genere</option>
+                </select>
+                <input class="form-control me-2" type="search" name="query" placeholder="Search" aria-label="Search">
                 <button class="btn btn-outline-success" type="submit">Search</button>
             </form>
             <ul>
                 <li><a href="index.php"><i class="bi bi-house"></i>Home</a></li>
-                <li><a href="profile.php"><i class="bi bi-person"></i>Profilo</a></li> <!-- in php linka a username proprio -->
+                <li><a href="profile.php"><i class="bi bi-person"></i>Profilo</a></li>
                 <li><a href="new-post.php"><i class="bi bi-plus-circle"></i>Nuovo Post</a></li>
             </ul>
         </nav>
--- template/processa-new-post.php
+++ template/processa-new-post.php
@@ -14,4 +14,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "Errore nella pubblicazione del post.";
     }
 }
-?>
