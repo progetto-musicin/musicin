f4ded77182205f067ae993af8927ad1121b0e2c0
buldra2000 <buldra00@icloud.com>
2024-06-19 19:12:05 +0200

    modifica new-post.php

--- template/new-post.php
+++ template/new-post.php
@@ -13,6 +13,10 @@
             <label for="attachment">Carica un'immagine:</label>
             <input type="file" accept="image/jpg, image/jpeg, image/png, image/gif" id="attachment" name="attachment">
         </li>
+        <li>
+            <label for="audio">Carica un brano audio:</label>
+            <input type="file" accept="audio/*" id="audio" name="audio">
+        </li>
     </ul>
     <input type="submit" value="Invia" />
     <input type="reset" value="Cancella" />
