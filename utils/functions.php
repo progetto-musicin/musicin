4efe342d9be79a93b09573bd17f4ba98f358f7a5
Simone Magnani <simone.magnani7@studio.unibo.it>
2024-06-24 11:17:34 +0200

    test upload functions

--- utils/functions.php
+++ utils/functions.php
@@ -9,6 +9,49 @@ function isUserLoggedIn() {
     return !empty(getCurrentUserId());
 }
 
+function uploadFile($path, $file, $acceptedExtensions) {
+    $fileName = basename($file["name"]);
+    $fullPath = $path.$fileName;
+
+    $maxKB = 500;
+    $result = 0;
+    $msg = "";
+
+    //Controllo dimensione dell'immagine < 500KB
+    if ($file["size"] > $maxKB * 1024) {
+        $msg .= "File caricato pesa troppo! Dimensione massima è $maxKB KB. ";
+    }
+
+    //Controllo estensione del file
+    if (!empty($acceptedExtensions)) {
+        $fileType = strtolower(pathinfo($fullPath,PATHINFO_EXTENSION));
+        if (!in_array($fileType, $acceptedExtensions)) {
+            $msg .= "Accettate solo le seguenti estensioni: ".implode(",", $acceptedExtensions);
+        }
+    }
+
+    //Controllo se esiste file con stesso nome ed eventualmente lo rinomino
+    if (file_exists($fullPath)) {
+        $i = 1;
+        do {
+            $i++;
+            $fileName = pathinfo(basename($file["name"]), PATHINFO_FILENAME)."_$i.".$fileType;
+        } while(file_exists($path.$fileName));
+        $fullPath = $path.$fileName;
+    }
+
+    //Se non ci sono errori, sposto il file dalla posizione temporanea alla cartella di destinazione
+    if (strlen($msg)==0) {
+        if (!move_uploaded_file($file["tmp_name"], $fullPath)) {
+            $msg.= "Errore nel caricamento dell'immagine.";
+        } else{
+            $result = 1;
+            $msg = $fileName;
+        }
+    }
+    return array($result, $msg);
+}
+
 // Funzione per caricare un'immagine in una cartella
 // Parametri:
 // - $path: percorso della cartella in cui caricare l'immagine
@@ -17,11 +60,6 @@ function isUserLoggedIn() {
 // - $result: 1 se l'upload è andato a buon fine, 0 altrimenti
 // - $msg: messaggio di errore se l'upload non è andato a buon fine, nome dell'immagine caricata altrimenti
 function uploadImage($path, $image) {
-    $imageName = basename($image["name"]);
-    $fullPath = $path.$imageName;
-
-    $maxKB = 500;
-    $acceptedExtensions = array("jpg", "jpeg", "png", "gif");
     $result = 0;
     $msg = "";
 
@@ -29,38 +67,14 @@ function uploadImage($path, $image) {
     $imageSize = getimagesize($image["tmp_name"]);
     if ($imageSize === false) {
         $msg .= "File caricato non è un'immagine! ";
-    }
-    //Controllo dimensione dell'immagine < 500KB
-    if ($image["size"] > $maxKB * 1024) {
-        $msg .= "File caricato pesa troppo! Dimensione massima è $maxKB KB. ";
+        return array($result, $msg);
     }
 
-    //Controllo estensione del file
-    $imageFileType = strtolower(pathinfo($fullPath,PATHINFO_EXTENSION));
-    if (!in_array($imageFileType, $acceptedExtensions)) {
-        $msg .= "Accettate solo le seguenti estensioni: ".implode(",", $acceptedExtensions);
-    }
+    return uploadFile($path, $image, array("jpg", "jpeg", "png", "gif"));
+}
 
-    //Controllo se esiste file con stesso nome ed eventualmente lo rinomino
-    if (file_exists($fullPath)) {
-        $i = 1;
-        do {
-            $i++;
-            $imageName = pathinfo(basename($image["name"]), PATHINFO_FILENAME)."_$i.".$imageFileType;
-        } while(file_exists($path.$imageName));
-        $fullPath = $path.$imageName;
-    }
-
-    //Se non ci sono errori, sposto il file dalla posizione temporanea alla cartella di destinazione
-    if (strlen($msg)==0) {
-        if (!move_uploaded_file($image["tmp_name"], $fullPath)) {
-            $msg.= "Errore nel caricamento dell'immagine.";
-        } else{
-            $result = 1;
-            $msg = $imageName;
-        }
-    }
-    return array($result, $msg);
+function uploadSong($path, $song) {
+    return uploadFile($path, $song, array());
 }
 
 ?>
