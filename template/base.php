4b0f0be6f0177db3667035021a9eae165996c88b
Simone Magnani <simone.magnani7@studio.unibo.it>
2024-06-26 17:19:16 +0200

    notification link text style change

--- template/base.php
+++ template/base.php
@@ -21,7 +21,7 @@
             <div class="container d-flex">
                 <h1><a href="/">Music.In</a></h1>
                 <div class="container nav nav-pills align-items-center justify-content-end">
-                    <a class="nav-link m-1 p-1 <?php markIfActive("notifications.php"); ?>" href="notifications.php"><i class="bi bi-bell"></i><span class="d-none d-sm-inline">Notifiche:</span></a>
+                    <a class="nav-link m-1 p-1 <?php markIfActive("notifications.php"); ?>" href="notifications.php"><i class="bi bi-bell"></i><span class="d-none d-sm-inline ms-1">Notifiche:</span></a>
                     <span id="notification_counter" class="p-1">0</span>
                     <!-- <span id="notification_counter" class="border border-info px-1 rounded-pill rounded-5">0</span> -->
                     <!-- <span id="notification_counter" class="border border-info p-1">0</span> -->
