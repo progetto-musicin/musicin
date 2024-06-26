c78b24e5e34ca1b0741ed50dfd41d1bc384889f8
buldra2000 <buldra00@icloud.com>
2024-06-26 17:37:13 +0200

    fixed processa-like.php

--- php/processa-like.php
+++ php/processa-like.php
@@ -7,7 +7,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['post_id'])) {
 
     
     if (!$dbh->doesUserLikePost($user_id, $post_id)) { //add like
-        // Aggiungi Mi Piace
+        
         $query = "INSERT INTO likes (post_id, user_id) VALUES (:post_id, :user_id)";
         $stmt = $dbh->prepare($query);
         $stmt->bindParam(':post_id', $post_id);
@@ -19,7 +19,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['post_id'])) {
         $dbh->createLikeNotification($user_id, $post_id);
 
         echo json_encode(['success' => true, 'newLikeCount' => $newLikeCount, 'liked' => true]);
-        
+
     } else {  //remove like
         
         $query = "DELETE FROM likes WHERE post_id = :post_id AND user_id = :user_id";
