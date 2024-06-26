b514a97d5a611607542f5a6ca0b70c8305fc436c
Simone Magnani <simone.magnani7@studio.unibo.it>
2024-06-26 12:36:06 +0200

    bootstrap footer test

--- template/base.php
+++ template/base.php
@@ -48,9 +48,11 @@
             <?php if(isset($templateParams["name"])) { require(__DIR__ . "/" . $templateParams["name"] . ".php"); } ?>
         </main>
 
-        <footer>
-            <p>Copyright &copy; 2024 Music.In</p>
+        <div class="container">
+            <footer class="py-3 my-4 border-top">
+            <p class="text-center text-body-secondary">Copyright &copy; 2024 Music.In</p>
         </footer>
+</div>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     </div>
