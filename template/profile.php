3a8ea6cd89a7243a5924843946ff505afdf1b4ab
Simone Magnani <simone.magnani7@studio.unibo.it>
2024-06-27 14:53:09 +0200

    fixes

--- template/profile.php
+++ template/profile.php
@@ -2,39 +2,36 @@
     <h2>Utente non trovato.</h2>
 <?php else: ?>
 <section>
-    <article>
-        <header>
-            <h2><?php echo htmlspecialchars($user['username']); ?></h2>
+    <h2><?php echo htmlspecialchars($user['username']); ?></h2>
 
-            <!-- Immagine Profilo -->
-            <img src="<?php echo UPLOAD_DIR . htmlspecialchars($user['profile_image']); ?>" alt="Immagine Profilo">
+    <!-- Immagine Profilo -->
+    <img src="<?php echo UPLOAD_DIR . htmlspecialchars($user['profile_image']); ?>" alt="Immagine Profilo">
 
-            <!-- Numero di Follower e Seguiti -->
-            <ul>
-                <li><a href="#">Followers: <?php echo htmlspecialchars($dbh->getNumFollowers($user_id)); ?></a></li>
-                <li><a href="#">Seguiti: <?php echo htmlspecialchars($dbh->getNumFollowing($user_id)); ?></a></li>
-            </ul>
+    <!-- Numero di Follower e Seguiti -->
+    <ul>
+        <li><a href="#">Followers: <?php echo htmlspecialchars($dbh->getNumFollowers($user_id)); ?></a></li>
+        <li><a href="#">Seguiti: <?php echo htmlspecialchars($dbh->getNumFollowing($user_id)); ?></a></li>
+    </ul>
 
 <?php if ($isMyProfile): ?>
-            <a href="edit-profile.php">Modifica Profilo</a>
+    <a href="edit-profile.php">Modifica Profilo</a>
 <?php endif; ?>
-            <ul>
+    <ul>
 <?php if (!empty($user["name"])): ?>
-                <li>Nome: <?php echo htmlspecialchars($user["name"]); ?></li>
+        <li>Nome: <?php echo htmlspecialchars($user["name"]); ?></li>
 <?php endif; ?>
 <?php if (!empty($user["surname"])): ?>
-                <li>Nome: <?php echo htmlspecialchars($user["surname"]); ?></li>
+        <li>Nome: <?php echo htmlspecialchars($user["surname"]); ?></li>
 <?php endif; ?>
 <?php if (!empty($user["genre_id"])): ?>
-                <li>Genere preferito: <?php echo htmlspecialchars($dbh->getGenreName($user["genre_id"])); ?></li>
+        <li>Genere preferito: <?php echo htmlspecialchars($dbh->getGenreName($user["genre_id"])); ?></li>
 <?php endif; ?>
-            </ul>
-        </header>
-    </article>
+    </ul>
 </section>
 
 <!-- Visualizzazione dei post -->
 <section class="post-grid">
+    <h2>Post</h2>
     <?php foreach ($posts as $post): ?>
         <?php require __DIR__ . "/post.php" ?>
     <?php endforeach; ?>
