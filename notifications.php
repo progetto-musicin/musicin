7804740166df8e907eea7a335a9404b59d073ed6
Simone Magnani <simone.magnani7@studio.unibo.it>
2024-06-28 14:11:05 +0200

    code style changes

--- notifications.php
+++ notifications.php
@@ -2,11 +2,10 @@
 
 require_once __DIR__ . '/bootstrap.php';
 
-$templateParams["title"] = "Music.In - Notifiche";
-$templateParams["name"] = "notifications";
-
 $notifications = $dbh->getNotifications($_SESSION["user_id"]);
 
+$templateParams["title"] = "Music.In - Notifiche";
+$templateParams["name"] = "notifications";
 require __DIR__ . '/template/base.php';
 
 ?>
