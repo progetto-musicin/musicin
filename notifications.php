f121b9553d39fce9cff01b499108d783f7f1d0a9
Simone Magnani <simone.magnani7@studio.unibo.it>
2024-06-28 14:06:51 +0200

    adjust title

--- notifications.php
+++ notifications.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . '/bootstrap.php';
 
-$templateParams["title"] = "Notifiche";
+$templateParams["title"] = "Music.In - Notifiche";
 $templateParams["name"] = "notifications";
 
 $notifications = $dbh->getNotifications($_SESSION["user_id"]);
