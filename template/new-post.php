3c612872b40a4e01bac433213bd6fcad5a644990
Simone Magnani <simone.magnani7@studio.unibo.it>
2024-07-01 22:21:27 +0200

    fix html

--- template/new-post.php
+++ template/new-post.php
@@ -1,9 +1,9 @@
 <form action="php/processa-new-post.php" method="post" enctype="multipart/form-data">
     <h2>Nuovo Post</h2>
-    <ul>
+    <div class="form-floating">
         <div class="mb-3">
             <label for="title" class="form-label">Titolo:</label>
-            <input type="text" class="form-control" id="title" name="title" placeholder="Dai un titolo al tuo post" required>
+            <input type="text" class="form-control" id="title" name="title" placeholder="Dai un titolo al tuo post" required />
         </div>
 
         <div class="mb-3">
@@ -13,15 +13,15 @@
 
         <div class="mb-3">
             <label for="attachment" class="form-label">Carica un immagine</label>
-            <input class="form-control" type="file" id="attachment" name="attachment" accept="image/jpg, image/jpeg, image/png, image/gif">
+            <input class="form-control" type="file" id="attachment" name="attachment" accept="image/jpg, image/jpeg, image/png, image/gif" />
         </div>
 
         <div class="mb-3">
             <label for="audio" class="form-label">Carica un brano</label>
-            <input class="form-control" type="file" id="audio" name="audio" accept="audio/*">
+            <input class="form-control" type="file" id="audio" name="audio" accept="audio/*" />
         </div>
 
-    </ul>
-    <input class="btn btn-primary" type="submit" value="Pubblica">
-    <input class="btn btn-primary" type="reset" value="Cancella">
+    </div>
+    <input class="btn btn-primary" type="submit" value="Pubblica" />
+    <input class="btn btn-primary" type="reset" value="Cancella" />
 </form>
