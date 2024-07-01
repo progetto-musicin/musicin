982d20da28d4af62971f72e50eca5497eb179004
Simone Magnani <simone.magnani7@studio.unibo.it>
2024-07-01 20:58:30 +0200

    html

--- template/concerts.php
+++ template/concerts.php
@@ -1,4 +1,4 @@
-<h1>Acquista con Ticketone</h1>
+<h2>Acquista con Ticketone</h2>
 <p>Voglia di un concerto? Scopri nuovi concerti in zona dei tuoi artisti preferiti</p>
 
 <iframe src="https://www.ticketone.it/events/concerti-55/" width="100%" height="2100px" scrolling="yes" frameborder="0" border="0"></iframe>
